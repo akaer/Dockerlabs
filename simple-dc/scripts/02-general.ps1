3b8ff145ab0e925408bdbfeb3a1ccc0bb8e7b399
André Raabe <andre.raabe@gmail.com>
2026-02-06 13:33:14 +0100

    Fix software installation

--- simple-dc/scripts/02-general.ps1
+++ simple-dc/scripts/02-general.ps1
@@ -11,32 +11,6 @@ $ProgressPreference = 'SilentlyContinue'
 
 $CustomTimeZone = 'W. Europe Standard Time'
 
-function Register-BGInfoStartup {
-
-        $bgInfo = Get-ChildItem -Path "$env:LocalAppData" -Filter 'BGInfo64.exe' -Recurse -Attributes !ReparsePoint
-
-        if (-not ($bgInfo)) {
-            Write-Host '[!] BGinfo not found!'
-
-            return
-        }
-
-        $bgInfo = $bgInfo.FullName
-
-        Copy-Item $bgInfo 'c:\windows\'
-        $exePath = 'c:\windows\BGInfo64.exe'
-
-        #Register Startup command for All User
-        $startupPath =  Join-Path $env:ProgramData 'Microsoft\Windows\Start Menu\Programs\Startup\BGInfo.lnk'
-        $shell = New-Object -COM WScript.Shell
-        $shortcut = $shell.CreateShortcut($startupPath)
-        $shortcut.TargetPath = $exePath
-        $shortcut.Arguments = 'c:\OEM\boxdefault.bgi /timer:0 /silent /nolicprompt'
-        $shortcut.Save()
-
-        Write-Host '[+] BGInfo registered for autostart'
-}
-
 Write-Host '[+] Configure defender'
 Set-MpPreference -DisableRealtimeMonitoring $true
 Set-MpPreference -DisableBehaviorMonitoring $true
@@ -186,27 +160,6 @@ Write-Host '[+] Update winget sources'
 if ($LASTEXITCODE -ne 0) {
     Write-Warning "[!] winget source update completed with exit code $LASTEXITCODE"
 }
-
-Write-Host '[+] Install global applications'
-& winget install --disable-interactivity --accept-package-agreements --accept-source-agreements --silent `
-Microsoft.Edit `
-7zip.7zip `
-Microsoft.DotNet.SDK.8 `
-Microsoft.DotNet.SDK.10 `
-Microsoft.PowerShell `
-Microsoft.Sysinternals.Suite `
-Notepad++.Notepad++ `
-2>&1 | ForEach-Object {
-    $line = "$_"
-    if ($line -match '^[\x21-\x7E]') {
-         Write-Host $line
-    }
-}
-if ($LASTEXITCODE -ne 0) {
-    Write-Warning "[!] winget install completed with exit code $LASTEXITCODE"
-}
-
-Register-BGInfoStartup
 
 $TargetScript = 'c:\OEM\addon-software.ps1'
 if (Test-Path "$TargetScript") {
---
+++ simple-dc/scripts/03-software.ps1
@@ -0,0 +1,59 @@
+﻿#Requires -Version 5.1
+#Requires -RunAsAdministrator
+
+Set-StrictMode -Version Latest
+if ($env:DEBUG -eq 'True') { Set-PSDebug -Trace 1 }
+
+$ErrorActionPreference = 'Stop'
+$InformationPreference = 'Continue'
+$DebugPreference = 'SilentlyContinue'
+$ProgressPreference = 'SilentlyContinue'
+
+function Register-BGInfoStartup {
+
+        $bgInfo = Get-ChildItem -Path "$env:LocalAppData" -Filter 'BGInfo64.exe' -Recurse -Attributes !ReparsePoint
+
+        if (-not ($bgInfo)) {
+            Write-Host '[!] BGinfo not found!'
+
+            return
+        }
+
+        $bgInfo = $bgInfo.FullName
+
+        Copy-Item $bgInfo 'c:\windows\'
+        $exePath = 'c:\windows\BGInfo64.exe'
+
+        #Register Startup command for All User
+        $startupPath =  Join-Path $env:ProgramData 'Microsoft\Windows\Start Menu\Programs\Startup\BGInfo.lnk'
+        $shell = New-Object -COM WScript.Shell
+        $shortcut = $shell.CreateShortcut($startupPath)
+        $shortcut.TargetPath = $exePath
+        $shortcut.Arguments = 'c:\OEM\boxdefault.bgi /timer:0 /silent /nolicprompt'
+        $shortcut.Save()
+
+        Write-Host '[+] BGInfo registered for autostart'
+}
+
+Write-Host '[+] Install global applications'
+& winget install --disable-interactivity --accept-package-agreements --accept-source-agreements --silent -e --source winget `
+Microsoft.Edit `
+7zip.7zip `
+Microsoft.DotNet.SDK.8 `
+Microsoft.DotNet.SDK.10 `
+Microsoft.PowerShell `
+Microsoft.Sysinternals.BGInfo `
+Microsoft.Sysinternals.Suite `
+Notepad++.Notepad++ `
+2>&1 | ForEach-Object {
+    $line = "$_"
+    if ($line -match '^[\x21-\x7E]') {
+         Write-Host $line
+    }
+}
+if ($LASTEXITCODE -ne 0) {
+    Write-Warning "[!] winget install completed with exit code $LASTEXITCODE"
+}
+
+Register-BGInfoStartup
+
